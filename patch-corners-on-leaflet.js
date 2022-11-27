02e7b5ff84d24fff4ad7f31740d6499f48de0b93
Alexey Belkin <48679054+avbelkin@users.noreply.github.com>
2022-11-28 00:45:48 +0300

    Add files via upload

---
+++ patch-corners-on-leaflet.js
@@ -0,0 +1,25 @@
+﻿L.Map.include({
+    _initControlPos: function () {
+        let corners = this._controlCorners = {},
+            l = 'leaflet-',
+            container = this._controlContainer =
+                L.DomUtil.create('div', l + 'control-container', this._container);
+
+        function createCorner(vSide, hSide) {
+            const className = l + vSide + ' ' + l + hSide;
+
+            corners[vSide + hSide] = L.DomUtil.create('div', className, container);
+        }
+        createCorner('top', 'center');
+        createCorner('middle', 'center');
+        createCorner('middle', 'left');
+        createCorner('middle', 'right');
+        createCorner('bottom', 'center');
+
+        createCorner('top', 'left');
+        createCorner('top', 'right');
+        createCorner('bottom', 'left');
+        createCorner('bottom', 'right');
+        createCorner('abso', 'lute');
+    }
+});
